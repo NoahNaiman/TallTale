a14a0613348b14eb8d4244f20584d72b0d203ca7
Jorge-Nario <jmnario@bu.edu>
2019-04-13 14:37:46 -0400

    finished setup shell script

--- init.sh
+++ init.sh
@@ -3,3 +3,11 @@
 git submodule init; git submodule update
 cd tacotron2
 git submodule init; git submodule update
+wget https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2
+
+tar -xjf  LJSpeech-1.1.tar.bz2
+
+mv LJSpeech-1.1 ljs_dataset_folder
+
+sed -i -- 's,DUMMY,ljs_dataset_folder/wavs,g' filelists/*.txt
+pip install -r requirements.txt
